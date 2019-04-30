6866ae2fc6deaa001cc3ce8c116429e4865ab498
Sagi <sagi.shmuel@gigya-inc.com>
2019-04-30 10:28:53 +0300

    fix retains cycle in login provider, finish web provider

---
+++ GigyaSwift/Global/Providers/Safari.temp_caseinsensitive_rename./WebViewController.swift
@@ -0,0 +1,9 @@
+//
+//  WebViewController.swift
+//  GigyaSwift
+//
+//  Created by Shmuel, Sagi on 29/04/2019.
+//  Copyright © 2019 Gigya. All rights reserved.
+//
+
+import Foundation
---
+++ GigyaSwiftTests/Providers/FacebookTests.swift
@@ -0,0 +1,9 @@
+//
+//  FacebookTests.swift
+//  GigyaSwiftTests
+//
+//  Created by Shmuel, Sagi on 28/04/2019.
+//  Copyright © 2019 Gigya. All rights reserved.
+//
+
+import Foundation
