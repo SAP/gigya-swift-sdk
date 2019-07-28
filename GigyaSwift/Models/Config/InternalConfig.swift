530a92a6546c3c744d937cc53187b8bc203b391b
Sagi <sagi.shmuel@gigya-inc.com>
2019-07-28 12:02:47 +0300

    #85023 update version to release

--- GigyaSwift/Models/Config/InternalConfig.swift
+++ GigyaSwift/Models/Config/InternalConfig.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct InternalConfig {
     struct General {
-        internal static let version = "ios_swift_1.0.0"
+        internal static let version = "ios_swift_1.0.1"
         internal static let sdkDomain = "com.gigya.GigyaSDK"
     }
 
