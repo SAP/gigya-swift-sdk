dc40291ee7703f97c5577dfb8e302e0169c37081
Sagi <sagi.shmuel@sap.com>
2020-07-22 18:01:14 +0300

    fix issue for pod

--- GigyaSwift/Global/Api/ApiService.swift
+++ GigyaSwift/Global/Api/ApiService.swift
@@ -1,121 +1,120 @@
 //
-//  ApiManager.swift
+//  GigyaRequest.swift
 //  GigyaSwift
 //
-//  Created by Shmuel, Sagi on 05/03/2019.
+//  Created by Shmuel, Sagi on 04/03/2019.
 //  Copyright © 2019 Gigya. All rights reserved.
 //
 
 import Foundation
 
-protocol IOCApiServiceProtocol {
-    var gigyaApi: IOCGigyaWrapperProtocol { get }
+//typealias GigyaResponse = GSResponse
+public typealias GigyaDictionary = [String: AnyCodable]
 
-    var sessionService: IOCSessionServiceProtocol { get }
+final class ApiService: ApiServiceProtocol {
 
-    var accountService: IOCAccountServiceProtocol { get }
+    let sessionService: SessionServiceProtocol?
 
-    init(gigyaApi: IOCGigyaWrapperProtocol, sessionService: IOCSessionServiceProtocol, accountService: IOCAccountServiceProtocol)
+    private let networkAdapter: NetworkAdapterProtocol?
 
-    func send(api: String, params: [String: String], complition: @escaping (GigyaApiResult<GigyaDictionary>) -> Void)
-
-    func send<T: Codable>(dataType: T.Type, api: String, params: [String: String], complition: @escaping (GigyaApiResult<T>) -> Void)
-
-    func login<T: Codable>(dataType: T.Type, loginId: String, password: String, complition: @escaping (GigyaApiResult<T>) -> Void)
-
-    func getAccount<T: Codable>(dataType: T.Type, complition: @escaping (GigyaApiResult<T>) -> Void)
-
-    func register<T: Codable>(params: [String: Any], dataType: T.Type, complition: @escaping (GigyaApiResult<T>) -> Void)
-}
-
-class BusinessApiService: IOCApiServiceProtocol {
-
-    let gigyaApi: IOCGigyaWrapperProtocol
-
-    var sessionService: IOCSessionServiceProtocol
-
-    var accountService: IOCAccountServiceProtocol
-
-//    lazy var service: NetworkProvider = {
-//        let service = NetworkProvider(url: appContext.config.apiDomain ?? InternalConfig.Storage.defaultApiDomain)
-//        return service
-//    }()d
-
-    required init(gigyaApi: IOCGigyaWrapperProtocol, sessionService: IOCSessionServiceProtocol, accountService: IOCAccountServiceProtocol) {
-        self.gigyaApi = gigyaApi
-        self.sessionService = sessionService
-        self.accountService = accountService
+    required init(with networkAdapter: NetworkAdapterProtocol, session: SessionServiceProtocol) {
+        self.networkAdapter = networkAdapter
+        self.sessionService = session
     }
 
-    // Send regular request
-    func send(api: String, params: [String: String] = [:], complition: @escaping (GigyaApiResult<GigyaDictionary>) -> Void ) {
-        let model = GigyaApiReguestModel(method: api, params: params)
-
-        gigyaApi.send(model: model, responseType: GigyaDictionary.self, completion: complition)
+    func send<T: Codable>(model: ApiRequestModel, responseType: T.Type,
+                          completion: @escaping (GigyaApiResult<T>) -> Void) {
+        send(model: model, responseType: responseType, blocking: false, completion: completion)
     }
 
-    // Send request with generic type.
-    func send<T: Codable>(dataType: T.Type, api: String, params: [String: String] = [:], complition: @escaping (GigyaApiResult<T>) -> Void ) {
-        let model = GigyaApiReguestModel(method: api, params: params)
-
-        gigyaApi.send(model: model, responseType: T.self, completion: complition)
-
+    func sendBlocking<T: Codable>(model: ApiRequestModel, responseType: T.Type,
+                          completion: @escaping (GigyaApiResult<T>) -> Void) {
+        send(model: model, responseType: responseType, blocking: true, completion: completion)
     }
 
-    func login<T: Codable>(dataType: T.Type, loginId: String, password: String, complition: @escaping (GigyaApiResult<T>) -> Void) {
-        let params = ["loginId": loginId, "password": password]
-        let model = GigyaApiReguestModel(method: GigyaDefinitions.API.login, params: params)
+    // Send request to server
+    private func send<T: Codable>(model: ApiRequestModel, responseType: T.Type, blocking: Bool,
+                          completion: @escaping (GigyaApiResult<T>) -> Void) {
+        networkAdapter?.send(model: model, blocking: blocking) { [weak self] (data, error) in
 
-        gigyaApi.send(model: model, responseType: T.self) { [weak accountService] result in
-            switch result {
-            case .success(let data):
-                accountService?.account = data
-                complition(result)
-            case .failure:
-                complition(result)
-            }
-        }
-    }
-
-    func getAccount<T: Codable>(dataType: T.Type, complition: @escaping (GigyaApiResult<T>) -> Void) {
-        if accountService.isCachedAccount() {
-            complition(.success(data: accountService.getAccount()))
-        } else {
-            let model = GigyaApiReguestModel(method: GigyaDefinitions.API.getAccountInfo)
-            
-            gigyaApi.send(model: model, responseType: T.self) { [weak accountService] result in
-                switch result {
-                case .success(let data):
-                    accountService?.account = data
-                    complition(result)
-                case .failure:
-                    complition(result)
+            if error == nil {
+                main { [weak self] in
+                    self?.validateResult(tmpData: model, responseType: responseType, data: data, completion: completion)
                 }
+                return
             }
+
+            GigyaLogger.log(with: self, message: "Error: \(String(describing: error?.localizedDescription))")
+
+            let error = error as NSError?
+
+            guard let code = error?.code, let callId = error?.userInfo["callId"] as? String else {
+                main { completion(.failure(NetworkError.networkError(error: error!))) }
+                return
+            }
+
+            let errorModel = GigyaResponseModel(statusCode: .unknown, errorCode: code,
+                                                      callId: callId,
+                                                      errorMessage: error?.localizedDescription,
+                                                      sessionInfo: nil,
+                                                      requestData: data as Data?)
+
+            completion(.failure(NetworkError.gigyaError(data: errorModel)))
         }
     }
 
-    func register<T: Codable>(params: [String: Any], dataType: T.Type, complition: @escaping (GigyaApiResult<T>) -> Void) {
-        let model = GigyaApiReguestModel(method: GigyaDefinitions.API.initRegistration)
-
-        gigyaApi.send(model: model, responseType: [String: AnyCodable].self) { [weak gigyaApi] (result) in
-            switch result {
-
-            case .success(let data):
-                let regToken = data["regToken"]?.value ?? ""
-                let makeParams: [String: Any] = ["regToken": regToken, "finalizeRegistration": "true"].merging(params) { $1 }
-
-                let model = GigyaApiReguestModel(method: GigyaDefinitions.API.register, params: makeParams)
-
-                gigyaApi?.send(model: model, responseType: T.self, completion: complition)
-
-            case .failure(let error):
-                complition(.failure(error))
-            }
+    // Validate and decode the result to GigyaApiResult
+    private func validateResult<T: Codable>(tmpData: ApiRequestModel? = nil, responseType: T.Type, data: NSData?,
+                                            completion: @escaping (GigyaApiResult<T>) -> Void) {
+        guard let data = data else {
+            GigyaLogger.log(with: self, message: "Error: data not found)")
+            main { completion(.failure(NetworkError.emptyResponse)) }
+            return
         }
+
+        do {
+            var gigyaResponse = try DecodeEncodeUtils.decode(fromType: GigyaResponseModel.self, data: data as Data)
+            gigyaResponse.requestData = data as Data
+
+            sessionService?.setSession(gigyaResponse.sessionInfo)
+
+            // retry when the error is request expired
+            if isRetryNeeded(with: gigyaResponse.errorCode) {
+                let retryDispacer = NetworkRetryDispacher<T>(networkAdapter: networkAdapter, tmpModel: tmpData!)
+                retryDispacer.startRetry { [weak self] (data) in
+                    self?.validateResult(responseType: T.self, data: data, completion: completion)
+                }
+                return
+            }
+
+            if gigyaResponse.errorCode == 0 {
+                do {
+                    let typedResponse = try DecodeEncodeUtils.decode(fromType: responseType.self, data: data as Data)
+
+                    GigyaLogger.log(with: self, message: "[Response]: \(typedResponse)")
+
+                    main { completion(GigyaApiResult.success(data: typedResponse)) }
+                } catch let error {
+                    GigyaLogger.log(with: self, message: error.localizedDescription)
+                    main { completion(.failure(NetworkError.jsonParsingError(error: error))) }
+                }
+
+            } else {
+                GigyaLogger.log(with: self, message: "Failed: \(gigyaResponse)")
+                main { completion(.failure(.gigyaError(data: gigyaResponse))) }
+            }
+
+        } catch let error {
+            GigyaLogger.log(with: self, message: error.localizedDescription)
+            main { completion(.failure(NetworkError.jsonParsingError(error: error))) }
+        }
+    }
+
+    private func isRetryNeeded(with errorCode: Int) -> Bool {
+        return errorCode == GigyaDefinitions.ErrorCode.requestExpired
     }
 
     deinit {
-        print("[ApiService]")
+//        self.tmpModel = nil
     }
 }
