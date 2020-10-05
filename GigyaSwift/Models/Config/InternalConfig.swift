6218d672abc0666cd1f8722aba1550ff80532658
Sagi <sagi.shmuel@sap.com>
2020-10-05 11:41:23 +0300

    update version

--- GigyaSwift/Models/Config/InternalConfig.swift
+++ GigyaSwift/Models/Config/InternalConfig.swift
@@ -10,7 +10,7 @@ import Foundation
 
 struct InternalConfig {
     struct General {
-        internal static let version = "ios_swift_1.1.4"
+        internal static let version = "ios_swift_1.1.5"
         internal static let sdkDomain = "com.gigya.GigyaSDK"
         internal static let defaultLang = "en"
     }
